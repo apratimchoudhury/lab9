645353fb51bde0d655aac7a1a553365957f2d3c0
apratimchoudhury <42172482+apratimchoudhury@users.noreply.github.com>
2018-11-03 23:11:04 +0530

    Create q11.cpp

---
+++ q11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,*p;
+    char b,*q;
+    double c,*r;
+    bool d,*s;
+    float e,*t;
+    cout<<"int variable size: "<<sizeof(a)<<"int pointer size: "<<sizeof(p)<<endl;
+    cout<<"char variable size: "<<sizeof(b)<<"int pointer size: "<<sizeof(q)<<endl;
+    cout<<"double variable size: "<<sizeof(c)<<"int pointer size: "<<sizeof(r)<<endl;
+    cout<<"bool variable size: "<<sizeof(d)<<"int pointer size: "<<sizeof(s)<<endl;
+    cout<<"float variable size: "<<sizeof(e)<<"int pointer size: "<<sizeof(t)<<endl;
+    return 0;
+}
